40b6920f1d889c86da7c90ee53a0d8ddf85acdbf
dokim1234 <dokim1234@gmail.com>
2022-08-17 09:23:13 -0400

    1

---
+++ scripts/script1.jsx
@@ -0,0 +1,10 @@
+//declare variables
+var playerScore = 0;
+var computerScore = 0;
+
+//logic for turns
+const whoGoesFirst = (player,computer) => {
+  let turn = Math.random();
+  return (turn < 0.5) ? player : computer;
+}
+
